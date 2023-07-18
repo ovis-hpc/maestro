ec7f2b4236ec63cddc22f84a867d0e644d93a0c6
nick <nick@ogc.us>
2023-07-18 17:48:49 -0500

    Add decomposition support to maestro
    Function fmt_cmd_args added to maestro_util to parse ldmsd command arguments

--- src/maestro/maestro_util.py
+++ src/maestro/maestro_util.py
@@ -114,6 +114,20 @@ def check_required(attr_list, container, container_name):
             raise ValueError("The '{0}' attribute is required in a {1}".
                              format(name, container_name))
 
+def fmt_cmd_args(comm, cmd, spec):
+    cfg_args = {}
+    cmd_attr_list = comm.get_cmd_attr_list(cmd)
+    for key in spec:
+        if key in cmd_attr_list['req'] or key in cmd_attr_list['opt']:
+            if key == 'plugin':
+                cfg_args[key] = spec[key]['name']
+                continue
+            cfg_args[key] = spec[key]
+    if not all(key in spec for key in cmd_attr_list['req']):
+        print(f'The attribute(s) {set(cmd_attr_list["req"]) - spec.keys()} are required by {cmd}')
+        raise ValueError()
+    return cfg_args
+
 def NUM_STR(obj):
     return str(obj) if type(obj) in [ int, float ] else obj
 
