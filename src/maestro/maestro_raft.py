bc4b1117165cbcf661284e9cc8dc7c16a92ffd83
nick <nick@ogc.us>
2023-12-05 17:19:59 -0600

    Create class "DaemonDict" for RAFT mode
    DaemonDict instance stores distributed nested dictionary of maestro instances
    and the LDMSD's they're responsible for
    Only used when maestro started in RAFT mode

---
+++ src/maestro/maestro_raft.py
@@ -0,0 +1,59 @@
+from pysyncobj import replicated, SyncObjConsumer
+
+class DaemonDict(SyncObjConsumer):
+    def __init__(self):
+        super(DaemonDict, self).__init__()
+        self.__daemons = {}
+
+    @replicated
+    def clear(self):
+        self.__daemons.clear()
+
+    @replicated
+    def __setitem__(self, mi, value):
+        self.__daemons[mi] = value
+
+    @replicated
+    def set(self, mi, value):
+        self.__daemons[mi] = value
+
+    @replicated
+    def addMaestroGroup(self, mi, data):
+        self.__daemons[mi] = data
+
+    @replicated
+    def setDaemonGroup(self, mi, group, data):
+        self.__daemons[mi][group] = data
+
+    @replicated
+    def setDaemon(self, mi, group, dmn, data):
+        self.__daemons[mi][group][dmn] = data
+
+    @replicated
+    def updateDaemonGroup(self, mi, group, data):
+        self.__daemons[mi][group].update(data)
+
+    @replicated
+    def updateDaemon(self, mi, group, dmn, data):
+        self.__daemons[mi][group][dmn].update(data)
+
+    def __getitem__(self, key):
+        return self.__daemons[key]
+
+    def __len__(self):
+        return len(self.__daemons)
+
+    def getMaestroGroup(self, mi):
+        return self.__daemons[mi]
+
+    def getDaemonGroup(self, mi, group):
+        return self.__daemons[mi][group]
+
+    def items(self):
+        return self.__daemons.items()
+
+    def keys(self):
+        return self.__daemons.keys()
+
+    def values(self):
+        return self.__daemons.values()
