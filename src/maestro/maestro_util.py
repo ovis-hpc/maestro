3d7563f41c5fc29e8c040da80bf98ce7f80499f1
nick <nick@ogc.us>
2024-12-16 10:59:32 -0600

    Adds advertisers and producer listeners
    Update updaters, storage policies, and producerse to support permissions
    Adds cache_ip to producers
    Refactor producer balancing to support advertisers

--- src/maestro/maestro_util.py
+++ src/maestro/maestro_util.py
@@ -1,3 +1,13 @@
+def dist_list(list_, n):
+    q, r = divmod(len(list_), n)
+    dist_list = []
+    idx = 0
+    for i in range(1, n + 1):
+        s = idx
+        idx += q + 1 if i <= r else q
+        dist_list.append(list_[s:idx])
+    return dist_list
+
 def fmt_cmd_args(comm, cmd, spec):
     cfg_args = {}
     cmd_attr_list = comm.get_cmd_attr_list(cmd)
